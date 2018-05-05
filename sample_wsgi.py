a0559bf1dc1e7ab2a817a4ea21cc14957186dde9
Abhishek Kumar <akimabhishek@gmail.com>
2018-05-06 01:30:58 +0530

    settings update

--- sample_wsgi.py
+++ sample_wsgi.py
@@ -6,7 +6,7 @@ path = '/home/akimabhishek/musicplayer'
 if path not in sys.path:
     sys.path.append(path)
 
-os.environ['DJANGO_SETTING_MODULE'] = 'musicplayer.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'musicplayer.settings'
 
 from django.core.wsgi import get_wsgi_application
 from django.contrib.staticfiles.handlers import StaticFilesHandler
